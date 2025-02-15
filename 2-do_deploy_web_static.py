f1ae443ec964d22cf7376ec132a6899b5da45fdb
D-Tr3x <owosenidaniel13@gmail.com>
2025-02-15 04:48:07 +0100

    Wrote a fabric script that distributes an archive to the web server, using the function `do_deploy`

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Fabfile based on thr file `1-pack_web_static.py` that distributes an archive
+to the web servers, using the function `do_deploy`
+"""
+from fabric.api import env, put, run
+import os
+from os.path import basename
+
+
+env.hosts = ['18.235.234.179', '54.160.94.251']
+env.user = 'ubuntu'
+env.key_filename = ['/home/d_trex/.ssh/id_rsa']
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers
+
+    Returns:
+        False if the file at the path `archive_path` doesn't exist
+        otherwise, True
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    filename = basename(archive_path)
+    folder_name = filename.split('.')[0]
+
+    try:
+        put(archive_path, '/tmp/')
+
+        run(f'mkdir -p /data/web_static/releases/{folder_name}/')
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename, folder_name))
+
+        run(f'mv /data/web_static/releases/{folder_name}/web_static/* '
+            f'/data/web_static/releases/{folder_name}/')
+
+        run(f'rm /tmp/{filename}')
+        run(f'rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(folder_name))
+        return True
+    except Exception as e:
+        return False
